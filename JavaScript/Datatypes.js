97fb269717e0ca7c7e7c55f617079a649092eb00
logzii123 <logzii123@gmail.com>
2024-06-18 07:12:05 +0000

    commit

---
+++ JavaScript/Datatypes.js
@@ -0,0 +1,9 @@
+let productPrize = 150000;
+let productDiscount = 15;
+let productName = 'Apple iPhone 11 Pro Max (512 GB, Blue)';
+let isAvailable = true;
+
+console.log(productPrize);
+console.log(productDiscount);
+console.log(productName);
+console.log(isAvailable);
