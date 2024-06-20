b899933a7535bc0064c4790e84769b5fe0e7cdb3
logzii123 <logzii123@gmail.com>
2024-06-20 04:32:58 +0000

    edited datatypes

--- JavaScript/Datatypes.js
+++ JavaScript/Datatypes.js
@@ -2,8 +2,13 @@ let productPrize = 150000;
 let productDiscount = 15;
 let productName = 'Apple iPhone 11 Pro Max (512 GB, Blue)';
 let isAvailable = true;
+let minusZero = -0;
 
 console.log(productPrize);
 console.log(productDiscount);
 console.log(productName);
 console.log(isAvailable);
+console.log(minusZero);
+
+let x;
+console.log(x); // undefined
