69a72984ae4f2ecc8be5c45b821ac4067e13d93c
logzii123 <logzii123@gmail.com>
2024-06-24 10:08:06 +0000

    added loops

---
+++ JavaScript/Loops/DoWhile.js
@@ -0,0 +1,6 @@
+let i = 1000;
+do {
+    console.log(i);
+    i += 1;
+} while(i <= 10);
+
---
+++ JavaScript/Loops/WhileDemo.js
@@ -0,0 +1,7 @@
+// print number from 1 to 10
+
+let i = 1;
+while (i <= 10) {
+    console.log(i);
+    i++;    
+}
