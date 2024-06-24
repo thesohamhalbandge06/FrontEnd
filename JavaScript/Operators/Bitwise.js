58ad1ccbf8c402e9bdb30d6af68a2248e6491773
logzii123 <logzii123@gmail.com>
2024-06-24 10:07:48 +0000

    added operators

---
+++ JavaScript/Operators/Bitwise.js
@@ -0,0 +1,9 @@
+const a = 5;
+const b = 6;
+
+console.log(a & b); // bitwise and
+console.log(a | 4); // bitwise or
+console.log(5 ^ 6); // bitwise xor
+console.log(5 << 3); // left shift
+console.log(1 << 3); // left shift 
+console.log(27 >> 2); // right shift 
