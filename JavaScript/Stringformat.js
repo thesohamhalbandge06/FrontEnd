7cd4e359cfcaf1ac6e1eba7c6b71c74731fabda3
logzii123 <logzii123@gmail.com>
2024-06-25 06:31:06 +0000

    commit

---
+++ JavaScript/Stringformat.js
@@ -0,0 +1,11 @@
+let s1 = `Raju said "Where are you going?"`;
+console.log(s1);
+
+let s2 = `I don't have money`;
+console.log(s2);
+
+let s3 = `Wow what a beautiful day don't feel like "going out"`;
+console.log(s3);
+
+let x = 30;
+console.log(`The value of x is ${x}`); // templated string
